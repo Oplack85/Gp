45be1ae115c130e1a99e4d5d0116c3791b699778
Oplack85 <hayd34500@gmail.com>
2024-09-19 00:38:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ if not TOKEN:
 # إنشاء بوت Telegram
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['openai'])
-def openai_command(message):
-    bot.send_message(message.chat.id, "اكتب سؤالك وسأجيب عليه إن شاء الله.")
-
 @bot.message_handler(content_types=['text'])
 def gptMessage(message):
-    try:
-        # إرسال الرسالة إلى دالة gpt واستلام الرد
-        resp = gpt(message.text)
-        bot.send_message(message.chat.id, f'<b>العقرب : {resp}</b>', parse_mode='HTML')
-    except Exception as e:
-        # التعامل مع الأخطاء وإرسال رسالة تنبيهية
-        bot.send_message(message.chat.id, f'حدث خطأ: {e}', parse_mode='HTML')
+    if message.text.startswith('/p '):
+        question = message.text[3:]  # استخرج السؤال بعد الأمر /p
+        try:
+            # إرسال الرسالة إلى دالة gpt واستلام الرد
+            resp = gpt(question)
+            bot.send_message(message.chat.id, f'<b>العقرب : {resp}</b>', parse_mode='HTML')
+        except Exception as e:
+            # التعامل مع الأخطاء وإرسال رسالة تنبيهية
+            bot.send_message(message.chat.id, f'حدث خطأ: {e}', parse_mode='HTML')
+    else:
+        bot.send_message(message.chat.id, "يرجى استخدام الأمر /p متبوعًا بسؤالك.")
 
 # بدء الاستماع للرسائل
 bot.infinity_polling()
