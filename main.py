8a9b586d5619847c231a7e21e2268dec1316f3c8
Oplack85 <hayd34500@gmail.com>
2024-09-23 12:43:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import threading
 from bs4 import BeautifulSoup
 
 # ضع هنا التوكن الخاص بالبوت
-API_TOKEN = 'YOUR_TELEGRAM_BOT_API_TOKEN'
+API_TOKEN = '7218686976:AAHKUWhhQFNIPfr12Yg0v08g7bti8OPdXsA'
 
 # إنشاء البوت باستخدام التوكن
 bot = telebot.TeleBot(API_TOKEN)
