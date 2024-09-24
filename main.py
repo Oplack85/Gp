a45d43770aaf355688d9b1b886305df489ed4ae6
Oplack85 <hayd34500@gmail.com>
2024-09-25 01:29:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import psycopg2
 import uuid
 from telethon import TelegramClient
+from telethon import events
 
 api_id = 23970174  # استبدل بـ API ID الخاص بك
 api_hash = 'f1db2e38b2c73448ef09c504187e888d'  # استبدل بـ API Hash الخاص بك
