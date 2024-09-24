63221741db9192f5ed4230c4b8a652e7a4ea3921
Oplack85 <hayd34500@gmail.com>
2024-09-24 22:33:18 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
-sudo apt install postgresql postgresql-contrib
+#!/bin/bash
+
+# تحديث قائمة الحزم
+sudo apt-get update
+
+# تثبيت PostgreSQL و PostgreSQL contrib
+sudo apt-get install -y postgresql postgresql-contrib
 python main.py
 echo "successfully. ✔️"
