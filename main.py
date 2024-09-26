c77f2253b1b7db2f63f7e31ac5a374ce794c1ee6
Oplack85 <hayd34500@gmail.com>
2024-09-27 01:19:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         [InlineKeyboardButton("استنساخ البريد", callback_data='clone_email')]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text('*[𝗖𝗽 𝗠𝗮𝗶𝗹 📬](t.me/Scorpion_scorp)\n\n✎┊‌ مرحبا بك في بوت استنساخ الايميلات 📧 \n\n✎┊‌ اضغط على الزر ادناه وٱتبع الخطوات ⬇️*', reply_markup=reply_markup, parse_mode='MarkdownV2', disable_web_page_preview=True)
-    
+    await update.message.reply_text('✎┊‌ اضغط على الزر لاستنساخ البريد:', reply_markup=reply_markup)
+
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     await query.answer()
     
     if query.data == 'clone_email':
-        await query.edit_message_text(text='✎┊‌ يرجى إدخال البريد الإلكتروني الذي تريد استنساخه:')
+        await update.message.reply_text('*[𝗖𝗽 𝗠𝗮𝗶𝗹 📬](t.me/Scorpion_scorp)\n\n✎┊‌ مرحبا بك في بوت استنساخ الايميلات 📧 \n\n✎┊‌ اضغط على الزر ادناه وٱتبع الخطوات ⬇️*', reply_markup=reply_markup, parse_mode='MarkdownV2', disable_web_page_preview=True)
         return EMAIL
 
 async def receive_email(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -31,13 +31,13 @@ async def receive_number(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         num_copies = int(update.message.text)
         if 1 <= num_copies <= 1000:
             original_email = context.user_data['original_email']
-            copies = [f"{original_email.split('@')[0]}{i}@{original_email.split('@')[1]}" for i in range(1, num_copies + 1)]
+            copies = [f"{i}) {original_email}" for i in range(1, num_copies + 1)]
             
             # تقسيم النسخ إلى مجموعات وإرسال كل مجموعة في رسالة منفصلة
-            chunk_size = 150  # عدد النسخ في كل رسالة
+            chunk_size = 50  # عدد النسخ في كل رسالة
             for i in range(0, len(copies), chunk_size):
                 copies_text = "\n".join(copies[i:i + chunk_size])
-                await update.message.reply_text(f'✎┊‌ تم إنشاء النسخ التالية: \n\n{copies_text}')
+                await update.message.reply_text(f'✎┊‌ تم إنشاء النسخ التالية:\n\n{copies_text}')
         else:
             await update.message.reply_text('✎┊‌ يرجى إدخال رقم بين 1 و 1000.')
             return NUMBER
@@ -77,3 +77,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
