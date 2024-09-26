5f0950bd5c142a320d40a6c10add278108c3336a
Oplack85 <hayd34500@gmail.com>
2024-09-26 19:55:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler, ConversationHandler, MessageHandler, filters
 
 # حالات المحادثة
-EMAIL, CONFIRM = range(2)
+EMAIL = range(1)
 
 def start(update: Update, context: CallbackContext) -> None:
     keyboard = [
@@ -22,7 +23,6 @@ def button(update: Update, context: CallbackContext) -> None:
 def receive_email(update: Update, context: CallbackContext) -> int:
     original_email = update.message.text
     # هنا يمكنك إضافة منطق لنسخ البريد إلى 50 نسخة
-    # مثلاً: for i in range(1, 51):  نسخ البريد
     update.message.reply_text(f'تم استنساخ البريد: {original_email} إلى 50 نسخة.')
     return ConversationHandler.END
 
@@ -31,14 +31,16 @@ def cancel(update: Update, context: CallbackContext) -> int:
     return ConversationHandler.END
 
 def main():
-    # استبدل 'YOUR_TOKEN' برمز التوكن الخاص بك
-    updater = Updater("7218686976:AAHbE6XlKHaiqW-GK8e-2LFPwCt_4Het-jc")
+    # الحصول على توكن البوت من المتغير البيئي
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    
+    updater = Updater(token)
 
     # إعداد ConversationHandler
     conv_handler = ConversationHandler(
         entry_points=[CallbackQueryHandler(button)],
         states={
-            EMAIL: [MessageHandler(Filters.text & ~Filters.command, receive_email)],
+            EMAIL: [MessageHandler(filters.TEXT & ~filters.COMMAND, receive_email)],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
